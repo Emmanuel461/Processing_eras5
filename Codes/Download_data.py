5888345bdc1103b12e14c46e9adc15ab624a1397
Emmanuel461 <jesusc461@gmail.com>
2024-07-17 19:41:43 +0100

    Update - First commit

---
+++ Codes/Download_data.py
@@ -0,0 +1,35 @@
+import cdsapi
+
+c = cdsapi.Client()
+
+c.retrieve(
+    'sis-agrometeorological-indicators',
+    {
+        'version': '1_1',
+        'format': 'zip',
+        'variable': '2m_temperature',
+        'statistic': 'day_time_mean',
+        'year': ['2023', '2024'],
+        'month': [
+            '01', '02', '03',
+            '04', '05', '06',
+            '07',
+        ],
+        'day': [
+            '01', '02', '03',
+            '04', '05', '06',
+            '07', '08', '09',
+            '10', '11', '12',
+            '13', '14', '15',
+            '16', '17', '18',
+            '19', '20', '21',
+            '22', '23', '24',
+            '25', '26', '27',
+            '28', '29', '30',
+            '31',
+        ],
+        'area': [
+            12.9, -16.8, 10,
+            14,
+        ],
+    }, 'download.zip')
